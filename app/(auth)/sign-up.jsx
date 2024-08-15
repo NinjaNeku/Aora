4e45e7a2ba02be41dc9c51b3b8393bdc9cdeee6d
Aayush <aayushgautam2502@gmail.com>
2024-08-15 15:20:26 +0530

    add: appwrite logic for sign in and sign up

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,12 +1,15 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants';
 import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton';
-import { Link } from 'expo-router';
+import { Link, useRouter } from 'expo-router';
+import { createUser } from '../../lib/appwrite';
+
 
 const SignUp = () => {
+  const router = useRouter()
   const [form, setForm] = useState({
     username:'',
     email: '',
@@ -15,9 +18,22 @@ const SignUp = () => {
 
   const[isSubmitting, setIsSubmitting] = useState(false)
 
-  const submit = () => {
+  const submit = async () => {
+    if(!form.username || !form.email || !form.password){
+      Alert.alert('Error','Please fill all fields')
+    }
+      setIsSubmitting(true);
 
-  }
+      try {
+        const result = await createUser(form.email, form.password, form.username)
+
+        router.replace("/home")
+      } catch (error) {
+        Alert.alert('Error', error.message) 
+      }finally{
+        setIsSubmitting(false)
+      }
+    }
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -51,7 +67,7 @@ const SignUp = () => {
             otherStyles="mt-7"
           />
           <CustomButton 
-            title="Sign In"
+            title="Sign Up"
             handlePress={submit}
             containerStyles="mt-7"
             isLoading={isSubmitting}
